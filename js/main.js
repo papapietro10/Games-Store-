128fe6072a0e56f52a02262d7d0aa4b2df5c15ed
Pedro Duboy <duboy36@gmail.com>
2022-06-04 16:45:50 -0300

    Subir proyecto

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let prueba1 = [`H`,`O`,`L`,`A`,'M','U','N','D','O' ];
+document.write('javascript: ');
+
+for(let i=0; i<=0; i++){
+    document.write(`${prueba1[0]}`);
+    document.write(`${prueba1[1]}`);
+    document.write(`${prueba1[2]}`);
+    document.write(`${prueba1[3]}`);
+    document.write(`${prueba1[4]}`);
+    document.write(`${prueba1[5]}`);
+    document.write(`${prueba1[6]}`);
+    document.write(`${prueba1[7]}`);
+    document.write(`${prueba1[8]}`);
+}
+
+
+let prueba2 = [`H`,`O`,`L`,`A`,'M','U','N','D','O' ];
+
+for(let i=0; i<=50; i++){
+    console.log(`${prueba2[0]}`);
+    console.log(`${prueba2[1]}`);
+    console.log(`${prueba2[2]}`);
+    console.log(`${prueba2[3]}`);
+    console.log(`${prueba2[4]}`);
+    console.log(`${prueba2[5]}`);
+    console.log(`${prueba2[6]}`);
+    console.log(`${prueba2[7]}`);
+    console.log(`${prueba2[8]}`);
+}
