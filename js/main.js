6df401b860df32d97b7e62bd30e3be6340c52153
Game-store <duboy36@gmail.com>
2022-06-29 20:01:54 -0300

    Formulario

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+
+/*Cards y carrito*/
 const carrito = [];
+
+//*DEPORTE*//
 const productos = [
 {id:1, titulo:'F1', precio: 18000, hayStock:false,img:"image/formula.webp"},
 {id:2, titulo:'NBA - 2K22', precio: 20000, hayStock:false, img:"image/PS4-NBA-2K22.webp"},
@@ -11,32 +15,32 @@ for (let i = 0; i < productos.length; i++){
 
 deporte += `<div class="col mb-5">
 <div class="card h-100">                     
-  <img class="card-img-top"  src=${productos[i].img} alt="..." />                
-  <div class="card-body p-4">
-    <div class="text-center">
-      <h5 class="fw-bolder">${productos[i].titulo}</h5>$
-      ${productos[i].precio}
-    </div>
-  </div>
-  <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-      <div class="text-center"><a class="btn btn-outline-dark mt-auto" onclick="agregarAlCarrito(${productos[i].id})" href="#">Add to cart</a></div>
-  </div>
+<img class="card-img-top"  src=${productos[i].img} alt="..." />                
+<div class="card-body p-4">
+<div class="text-center">
+<h5 class="fw-bolder">${productos[i].titulo}</h5>$
+${productos[i].precio}
+</div>
+</div>
+<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+<div class="text-center"><a class="btn btn-outline-dark mt-auto" onclick="agregarAlCarrito(${productos[i].id})" href="#">Add to cart</a></div>
+</div>
 </div>
 </div>`;
 }
 
 function agregarAlCarrito(id){
-  const calculo = productos.findIndex(producto => producto.id == id);
-  carrito.push(productos[calculo]);
-  console.log( 'Tenes ' + carrito.length + ' juego en el carrito');
-  console.log(carrito)
-  const totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto. precio, 0 );
-  document.getElementById('cuenta').innerHTML = carrito.length + "- $" + totalCarrito;
+const calculo = productos.findIndex(producto => producto.id == id);
+carrito.push(productos[calculo]);
+console.log( 'Tenes ' + carrito.length + ' juego en el carrito');
+console.log(carrito)
+const totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto. precio, 0 );
+document.getElementById('cuenta').innerHTML = carrito.length + "- $" + totalCarrito;
 
-  }
+}
 
 document.getElementById("card-container-deporte").innerHTML = deporte
- 
+
 //*AVENTURA*//
 
 
@@ -52,29 +56,29 @@ for (let i = 0; i < productosAventura.length; i++){
 
 aventura += `<div class="col mb-5">
 <div class="card h-100">                     
-  <img class="card-img-top"  src=${productosAventura[i].img} alt="..." />                
-  <div class="card-body p-4">
-    <div class="text-center">
-      <h5 class="fw-bolder">${productosAventura[i].titulo}</h5>$
-      ${productosAventura[i].precio}
-    </div>
-  </div>
-  <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-      <div class="text-center"><a class="btn btn-outline-dark mt-auto" onclick="agregarAlCarrito(${productosAventura[i].id})" href="#">Add to cart</a></div>
-  </div>
+<img class="card-img-top"  src=${productosAventura[i].img} alt="..." />                
+<div class="card-body p-4">
+<div class="text-center">
+<h5 class="fw-bolder">${productosAventura[i].titulo}</h5>$
+${productosAventura[i].precio}
+</div>
+</div>
+<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+<div class="text-center"><a class="btn btn-outline-dark mt-auto" onclick="agregarAlCarrito(${productosAventura[i].id})" href="#">Add to cart</a></div>
+</div>
 </div>
 </div>`;
 }
 
 function agregarAlCarrito(id){
-  const calculo = productosAventura.findIndex(producto => producto.id == id);
-  carrito.push(productosAventura[calculo]);
-  console.log( 'Tenes ' + carrito.length + ' juego en el carrito');
-  console.log(carrito)
-  const totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto. precio, 0 );
-  document.getElementById('cuenta').innerHTML = carrito.length + "- $" + totalCarrito;
+const calculo = productosAventura.findIndex(producto => producto.id == id);
+carrito.push(productosAventura[calculo]);
+console.log( 'Tenes ' + carrito.length + ' juego en el carrito');
+console.log(carrito)
+const totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto. precio, 0 );
+document.getElementById('cuenta').innerHTML = carrito.length + "- $" + totalCarrito;
 
-  }
+}
 
 document.getElementById("card-container-aventura").innerHTML = aventura;
 
@@ -82,85 +86,84 @@ document.getElementById("card-container-aventura").innerHTML = aventura;
 
 
 const productosAccion = [
-  {id:1, titulo:'GTA', precio: 18000, categoria:'accion', hayStock:false,img:"image/357841-1600-auto.webp"},
-  {id:2, titulo:'Call of Duty', precio: 20000, categoria:'accion', hayStock:false,img:"image/combo3.jpeg"},
-  {id:3, titulo:'Ghost', precio: 12000,categoria:'accion', hayStock:false,img:"image/S5_GoTDC_R--2-.webp"},
-  {id:4, titulo:'VIII AGE', precio: 20000,categoria:'accion', hayStock:false,img:"image/357857-1600-auto.webp"},
-  ];
-  
-  let accion = ``;
-  for (let i = 0; i < productosAccion.length; i++){
-  
-  accion+= `<div class="col mb-5">
-  <div class="card h-100">                     
-    <img class="card-img-top"  src=${productosAccion[i].img} alt="..." />                
-    <div class="card-body p-4">
-      <div class="text-center">
-        <h5 class="fw-bolder">${productosAccion[i].titulo}</h5>$
-        ${productosAccion[i].precio}
-      </div>
-    </div>
-    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-        <div class="text-center"><a class="btn btn-outline-dark mt-auto" onclick="agregarAlCarrito(${productos[i].id})" href="#">Add to cart</a></div>
-    </div>
-  </div>
-  </div>`;
-  }
-  
-  function agregarAlCarrito(id){
-    const calculo = productosAccion.findIndex(producto => producto.id == id);
-    carrito.push(productosAccion[calculo]);
-    console.log( 'Tenes ' + carrito.length + ' juego en el carrito');
-    console.log(carrito)
-    const totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto. precio, 0 );
-    document.getElementById('cuenta').innerHTML = carrito.length + "- $" + totalCarrito;
-  
-    }
-  
-  document.getElementById("card-container-accion").innerHTML = accion;
+{id:1, titulo:'GTA', precio: 18000, categoria:'accion', hayStock:false,img:"image/357841-1600-auto.webp"},
+{id:2, titulo:'Call of Duty', precio: 20000, categoria:'accion', hayStock:false,img:"image/combo3.jpeg"},
+{id:3, titulo:'Ghost', precio: 12000,categoria:'accion', hayStock:false,img:"image/S5_GoTDC_R--2-.webp"},
+{id:4, titulo:'VIII AGE', precio: 20000,categoria:'accion', hayStock:false,img:"image/357857-1600-auto.webp"},
+];
 
-  //*COMBOS*//
+let accion = ``;
+for (let i = 0; i < productosAccion.length; i++){
 
+accion+= `<div class="col mb-5">
+<div class="card h-100">                     
+<img class="card-img-top"  src=${productosAccion[i].img} alt="..." />                
+<div class="card-body p-4">
+<div class="text-center">
+<h5 class="fw-bolder">${productosAccion[i].titulo}</h5>$
+${productosAccion[i].precio}
+</div>
+</div>
+<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+<div class="text-center"><a class="btn btn-outline-dark mt-auto" onclick="agregarAlCarrito(${productos[i].id})" href="#">Add to cart</a></div>
+</div>
+</div>
+</div>`;
+}
+
+function agregarAlCarrito(id){
+const calculo = productosAccion.findIndex(producto => producto.id == id);
+carrito.push(productosAccion[calculo]);
+console.log( 'Tenes ' + carrito.length + ' juego en el carrito');
+console.log(carrito)
+const totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto. precio, 0 );
+document.getElementById('cuenta').innerHTML = carrito.length + "- $" + totalCarrito;
+
+}
+
+document.getElementById("card-container-accion").innerHTML = accion;
+
+//*COMBOS*//
 
 const productosCombos = [
-  {id:1, titulo:'Combo 1', precio: 50000, hayStock:false, img:"image/combo2.jpeg"},
-  {id:2, titulo:'Combo 2', precio: 80000, categoria:'Deporte', hayStock:false, img:"image/combo1.webp"},
-  {id:3, titulo:'combo 3', precio: 70000,categoria:'Deporte', hayStock:false, img:"image/213590-1200-auto.webp"},
-  {id:4, titulo:'Combo 4', precio: 60000,categoria:'Deporte', hayStock:false, img:"image/196114-1200-auto.webp"},
-  ];
-  
-  let combos = ``;
-  for (let i = 0; i < productosCombos.length; i++){
-  
- combos+= `<div class="col mb-5">
-  <div class="card h-100">                     
-    <img class="card-img-top"  src=${productosCombos[i].img} alt="..." />                
-    <div class="card-body p-4">
-      <div class="text-center">
-        <h5 class="fw-bolder">${productosCombos[i].titulo}</h5>$
-        ${productosCombos[i].precio}
-      </div>
-    </div>
-    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-        <div class="text-center"><a class="btn btn-outline-dark mt-auto" onclick="agregarAlCarrito(${productosCombos[i].id})" href="#">Add to cart</a></div>
-    </div>
-  </div>
-  </div>`;
-  }
-  
-  function agregarAlCarrito(id){
-    const calculo = productosCombos.findIndex(producto => producto.id == id);
-    carrito.push(productosCombos[calculo]);
-    console.log( 'Tenes ' + carrito.length + ' juego en el carrito');
-    console.log(carrito)
-    const totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto. precio, 0 );
-    document.getElementById('cuenta').innerHTML = carrito.length + "- $" + totalCarrito;
-  
-    }
-  
-  document.getElementById("card-container-combos").innerHTML = combos;
+{id:1, titulo:'Combo 1', precio: 50000, hayStock:false, img:"image/combo2.jpeg"},
+{id:2, titulo:'Combo 2', precio: 80000, categoria:'Deporte', hayStock:false, img:"image/combo1.webp"},
+{id:3, titulo:'combo 3', precio: 70000,categoria:'Deporte', hayStock:false, img:"image/213590-1200-auto.webp"},
+{id:4, titulo:'Combo 4', precio: 60000,categoria:'Deporte', hayStock:false, img:"image/196114-1200-auto.webp"},
+];
 
-  const enviar = document.getElementById("enviar");
+let combos = ``;
+for (let i = 0; i < productosCombos.length; i++){
+
+combos+= `<div class="col mb-5">
+<div class="card h-100">                     
+<img class="card-img-top"  src=${productosCombos[i].img} alt="..." />                
+<div class="card-body p-4">
+<div class="text-center">
+<h5 class="fw-bolder">${productosCombos[i].titulo}</h5>$
+${productosCombos[i].precio}
+</div>
+</div>
+<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+<div class="text-center"><a class="btn btn-outline-dark mt-auto" onclick="agregarAlCarrito(${productosCombos[i].id})" href="#">Add to cart</a></div>
+</div>
+</div>
+</div>`;
+}
+
+function agregarAlCarrito(id){
+const calculo = productosCombos.findIndex(producto => producto.id == id);
+carrito.push(productosCombos[calculo]);
+console.log( 'Tenes ' + carrito.length + ' juego en el carrito');
+console.log(carrito)
+const totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto. precio, 0 );
+document.getElementById('cuenta').innerHTML = carrito.length + "- $" + totalCarrito;
+}
+document.getElementById("card-container-combos").innerHTML = combos;
+
+
+//*Evento en el bt de enviar*//
+const enviar = document.getElementById("enviar");
 
 enviar.onclick = () =>{
 const nombre = document.getElementById("nombre").value;
@@ -168,7 +171,7 @@ const mail = document.getElementById("mail").value;
 const apellido = document.getElementById("apellido").value;
 
 
-alert(`Hola recibimos tu consulta ${nombre}. Nos estaremos contactando a traves del siguiente email ${mail} Muchas gracias sr ${apellido} `)
-  }
+alert(`Hola ${nombre}! 👋🏻 recibimos tu consulta. Nos estaremos contactando a traves del siguiente email 👉🏻 ${mail} Muchas gracias sr ${apellido} 👾`)
+}
 
   
